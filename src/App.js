96dacb5fb64a81d97ad34bad5bc3956558e24ad4
kyh012412 <kyh0124.12@gmail.com>
2024-03-06 16:44:14 +0900

    올바른 useMemo 작동중

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import './App.css';
 
 const hardCalculate = (number) => {
@@ -12,12 +12,20 @@ const easyCalculate = (number) =>{
   return number+1;
 }
 
-
 function App() {
   const [hardNumber, setHardNumber] = useState(1);
   const [easyNumber, setEasyNumber] = useState(1);
 
-  const hardSum = hardCalculate(hardNumber);
+  // const hardSum = hardCalculate(hardNumber);
+
+  /**
+   * 두번째 인자에 있는 값이 바뀔때만 다시 계산을 한다.
+   * 현재의 경우 hardNumber이 바뀔때만 hardSum을 다시계산
+   */
+  const hardSum = useMemo(()=>{
+    return hardCalculate(hardNumber);
+  },[hardNumber]);
+  
   const easySum = easyCalculate(easyNumber);
 
   return (
@@ -29,7 +37,9 @@ function App() {
         onChange={(e) => setHardNumber(parseInt(e.target.value))}
       />
       <span> + 10000 = {hardSum}</span>
+
       <hr></hr>
+
       <h3>쉬운 계산기</h3>
       <input
         type="number"
